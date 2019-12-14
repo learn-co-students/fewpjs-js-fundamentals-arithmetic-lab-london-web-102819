931b78a42bb51b11b7afd64c57e0f035386a1658
Qing <chingwang.qw@gmail.com>
2019-12-14 09:33:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 1
+let num2 = 62
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 100);
+
+let mod = 53 % 7
+
+let max = Math.max(1, 2, 3, 4, 20)
