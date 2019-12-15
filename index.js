09d7cb6120b0bbb838e514c2edf7d24be47ae1e0
Anastasia Pankina <siapankina@gmail.com>
2019-12-15 19:01:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 9) + 1;
+let mod = 16 % 6;
+let max = Math.max(7, 4, 8, 10, 3, 4, 20)
+
